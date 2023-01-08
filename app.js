be6a1a4187ba9772a21485961046995ee3f06edb
Marta <m.marta.majewska@gmail.com>
2023-01-08 12:19:21 +0000

    Handle demo path

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/demo', (req, res) => {
+  res.set('X-full-stac', '4life');
+  res.status(418);
+  res.send('I prefere coffee');
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
